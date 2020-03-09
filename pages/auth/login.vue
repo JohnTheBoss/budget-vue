b8f86d13246615913e0dfeab3e9d99b8bc4baa21
JohnTheBoss <git@janoskollar.hu>
2020-03-09 11:11:38 +0100

    auth forms design

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,31 +1,44 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="signInUser()">
-      <div class="form-group">
-        <input v-model="formData.email" type="email" class="form-control" placeholder="Email" />
-      </div>
-
-      <div class="form-group">
-        <input
-          v-model="formData.password"
-          type="password"
-          class="form-control"
-          placeholder="Password"
-        />
-      </div>
-
-      <div class="form-group">
-        <button type="submit">Login</button>
-      </div>
+  <div class="text-center">
+    <form class="form-signin" @submit.prevent="signInUser()">
+      <img class="mb-4" src="~assets/logo.png" alt="Logo" width="72" height="72" />
+      <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control mb-2"
+        placeholder="Email"
+        v-model="formData.email"
+        required
+        autofocus
+      />
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+         v-model="formData.password"
+        required
+      />
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+      <p class="mt-5 mb-3 text-muted">New user? <nuxt-link to="/auth/signup">SignUp</nuxt-link> to create your account.</p>
+      <p class="mt-2 mb-3 text-muted">&copy; 2020 BudgetApp</p>
     </form>
-    <nuxt-link to="/auth/signup">SignUp</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
   middleware: "guest",
+  head() {
+    return {
+      bodyAttrs: {
+        class: "auth-page"
+      }
+    };
+  },
   data: () => ({
     formData: {
       email: "",
@@ -50,4 +63,5 @@ export default {
 </script>
 
 <style>
+
 </style>
--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,44 +1,69 @@
 <template>
-  <div>
-    <h1>SignUP</h1>
-    <form @submit.prevent="createUser()">
-      <div class="form-group">
-        <input v-model="formData.name" type="text" class="form-control" placeholder="Name" />
-      </div>
+  <div class="text-center">
+    <form class="form-signin" @submit.prevent="createUser()">
+      <img class="mb-4" src="~assets/logo.png" alt="Logo" width="72" height="72" />
+      <h1 class="h3 mb-3 font-weight-normal">SignUp</h1>
 
-      <div class="form-group">
-        <input v-model="formData.email" type="email" class="form-control" placeholder="Email" />
-      </div>
+      <label for="inputName" class="sr-only">Name</label>
+      <input
+        type="text"
+        id="inputName"
+        class="form-control mb-2"
+        placeholder="Name"
+        v-model="formData.name"
+        required
+        autofocus
+      />
 
-      <div class="form-group">
-        <input
-          v-model="formData.password"
-          type="password"
-          class="form-control"
-          placeholder="Password"
-        />
-      </div>
+      <label for="inputEmail" class="sr-only">Email</label>
+      <input
+        type="email"
+        id="inputEmail"
+        class="form-control mb-2"
+        placeholder="Email"
+        v-model="formData.email"
+        required
+      />
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control mb-2"
+        placeholder="Password"
+        v-model="formData.password"
+        required
+      />
 
-      <div class="form-group">
-        <input
-          v-model="formData.passwordAgain"
-          type="password"
-          class="form-control"
-          placeholder="Password Again"
-        />
-      </div>
-
-      <div class="form-group">
-        <button type="submit">SignUp</button>
-      </div>
+      <label for="inputPasswordAgain" class="sr-only">Password Again</label>
+      <input
+        type="password"
+        id="inputPasswordAgain"
+        class="form-control"
+        placeholder="Password Again"
+        v-model="formData.passwordAgain"
+        required
+      />
+      <button class="btn btn-lg btn-primary btn-block" type="submit">SignUp</button>
+      <p class="mt-5 mb-3 text-muted">
+        Already have an account?
+        <nuxt-link to="/auth/login">Sign In</nuxt-link>
+      </p>
+      <p class="mt-2 mb-3 text-muted">&copy; 2020 BudgetApp</p>
     </form>
-    <nuxt-link to="/auth/login">Login</nuxt-link>
   </div>
 </template>
+
 
 <script>
 export default {
   middleware: "guest",
+  head() {
+    return {
+      bodyAttrs: {
+        class: "auth-page"
+      }
+    };
+  },
   data: () => ({
     formData: {
       email: "",
@@ -51,12 +76,17 @@ export default {
     async createUser() {
       try {
         await this.$fireAuth
-          .createUserWithEmailAndPassword(this.formData.email, this.formData.password)
-          .then(function(result) {
-            return result.user.updateProfile({
-              displayName: this.formData.name
-            });
-          }.bind(this))
+          .createUserWithEmailAndPassword(
+            this.formData.email,
+            this.formData.password
+          )
+          .then(
+            function(result) {
+              return result.user.updateProfile({
+                displayName: this.formData.name
+              });
+            }.bind(this)
+          )
           .catch(function(error) {
             console.log(error);
           });
